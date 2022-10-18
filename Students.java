08030e3e343d4abfd0148dd50062363a5389d5d0
prashanth <conceptualsoftwaretrainings@gmail.com>
2022-10-18 21:27:18 +0530

    First commit - Only Source Code

---
+++ Students.java
@@ -0,0 +1,9 @@
+public class Students
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello there, \nWelcome to DevOps Sessions");
+		System.out.println("Students joined the session on 10-18-2022");
+		System.out.println("Addy\nAzeez\nMohamed\nPavan\nPuru\nSainath\nVidya\nVijay");
+	}
+}
