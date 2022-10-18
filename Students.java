2f613fced57ef343d66f88ff8e364bac521959f7
Prashanth <106029409+nocturnaldevops@users.noreply.github.com>
2022-10-18 21:49:38 +0530

    New change- added student azeez

--- Students.java
+++ Students.java
@@ -4,6 +4,6 @@ public class Students
 	{
 		System.out.println("Hello there, \nWelcome to DevOps Sessions");
 		System.out.println("Students joined the session on 10-18-2022");
-		System.out.println("Addy\nMohamed\nPavan\nPuru\nSainath\nVidya\nVijay");
+		System.out.println("Addy\nAzeez\nMohamed\nPavan\nPuru\nSainath\nVidya\nVijay");
 	}
 }
